81ed16f7a793eb8bf65823be306fc7e492698c4d
Gleb Zavizenov <gleb.zavizenov@gmail.com>
2019-05-29 21:51:31 -0400

    Added a custom text field

---
+++ window-shopper/window-shopper/View/CurrencyTextField.swift
@@ -0,0 +1,26 @@
+//
+//  CurrencyTextField.swift
+//  window-shopper
+//
+//  Created by Gleb Zavizenov on 2019-05-29.
+//  Copyright © 2019 Gleb Zavizenov. All rights reserved.
+//
+
+import UIKit
+
+class CurrencyTextField: UITextField {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0.25)
+        layer.cornerRadius = 5.0
+        textAlignment = .center
+        
+        if let p = placeholder{
+            let place = NSAttributedString(string: p, attributes: [.foregroundColor : UIColor.white])
+            attributedPlaceholder = place
+            textColor = UIColor.white
+        }
+    }
+
+}
